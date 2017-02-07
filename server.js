64d6c6321f0dc6811479dd64d535199a22ddcca7
kenny <kenny.selorm@gmail.com>
2017-02-06 23:44:11 -0800

    modified http

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const PORT  = process.env.PORT || 3000;
 
 //openweathermap
 app.use(function (req,res,next){
-  if (req.headers['x-forwarded-proto']==='http'){
-    next();
+  if (req.headers['x-forwarded-proto']==='https'){
+      res.redirect('http://'+req.hostname+req.url);
+
 
   }else{
-    res.redirect('http://'+req.hostname+req.url);
+    next();
+
 
   }
 
