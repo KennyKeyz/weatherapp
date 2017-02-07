7f4cdd502b67b211e421928161d1b6a8b0c8457c
kenny <kenny.selorm@gmail.com>
2017-02-06 18:29:15 -0800

    prepare repo for heroku

--- server.js
+++ server.js
@@ -4,8 +4,22 @@ var express  = require('express');
 
 var app = express();
 
+const PORT  = process.env.PORT || 3000;
+
+//openweathermap
+app.use(function (req,res,nextt){
+  if (req.headers['x-forwarded-proto']==='http'){
+    next();
+
+  }else{
+    res.redirect('http://'+req.hostname+req.url);
+
+  }
+
+});
+
 app.use(express.static('public'));
 
-app.listen(3000,function (){
-  console.log('Express server is up on port 3000');
+app.listen(PORT,function (){
+  console.log('Express server is up on port '+ PORT);
 });
